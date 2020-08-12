f8f0d5b444463d014deca261500a9b3f6c447891
Anjum-Shaik <69392607+Anjum-Shaik@users.noreply.github.com>
2020-08-12 15:41:20 +0530

    Add files via upload
    steps involved:
    Firstly, it  captures and  stores the background frame.
    Second, it detects the defined color using color detection and segmentation algorithm.
    Now it segment out the defined colored part by the generation of a mask.
    Finally, it generates the augmented output to create a magical effect.

---
+++ Invisible.py
@@ -0,0 +1,36 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+[1 2 3 5]
+<class 'numpy.ndarray'>
+>>> 
+>>> 
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+Traceback (most recent call last):
+  File "C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py", line 2, in <module>
+    arr=np.array([1,2,3,5],[5,9,8,0])
+TypeError: data type not understood
+>>> 
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+[[1 2 3 5]
+ [5 9 8 0]]
+>>> 
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+42
+>>> 
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
+
+ RESTART: C:/Users/Allabakash Shaik/AppData/Local/Programs/Python/Python37/hhh.py 
